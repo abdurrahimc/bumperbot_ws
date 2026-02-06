285693bb208cc15aa8789160c89a9f81c993c543
Abdurrahim Caliskan <abdurrahimc165@gmail.com>
2026-02-06 15:14:55 +0300

    launch

--- build/bumperbot_description/cmake_install.cmake
+++ build/bumperbot_description/cmake_install.cmake
@@ -46,6 +46,7 @@ if("x${CMAKE_INSTALL_COMPONENT}x" STREQUAL "xUnspecifiedx" OR NOT CMAKE_INSTALL_
   file(INSTALL DESTINATION "${CMAKE_INSTALL_PREFIX}/share/bumperbot_description" TYPE DIRECTORY FILES
     "/home/saul/bumperbot_ws/src/bumperbot_description/meshes"
     "/home/saul/bumperbot_ws/src/bumperbot_description/urdf"
+    "/home/saul/bumperbot_ws/src/bumperbot_description/rviz"
     )
 endif()
 
